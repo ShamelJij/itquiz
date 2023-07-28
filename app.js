10227450ad16eeff67f9facb4e6391217263ad8a
shameljij <shamel.jij@^gmail.com>
2023-07-28 14:41:53 +0200

    have to make changes to json!

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ request.open("GET", "test.json", false);
 request.send(null);
 let myObj = JSON.parse(request.responseText);
 console.log(myObj);
+console.log(myObj.forEach( o => console.log(o.id)));
 let element = document.getElementById("foodtitle");
 let ranMax = myObj.length;
 
 function getRanId(min,max){
-  return Math.round(Math.random() * (max - min) ) + min;
+  return Math.round(Math.random() * (max - min + 1) ) + min;
+}
+const ranNum = getRanId(16, ranMax);
+//ranNum === 10 ? console.log('matches!!') : console.log('doesnt match');
+let ranInquery = "";
+function getInquery(obj){
+ranInquery = JSON.stringify(obj.find((o) => o.id === ranNum ? (ranInquery = o.id.replace(/^["'](.+(?=["']$))["']$/, '$1') ,console.log('mat!!')) : console.log("object number doesn't match")));
+console.log(ranInquery);
 }
 function showEle(){
   const ranNum = getRanId(1, ranMax);
@@ -19,4 +27,5 @@ function showEle(){
   //element.innerHTML = apar;
 console.log("ranNum: " , ranNum , " ranMax: " , ranMax);
 }
-showEle();
+//showEle();
+getInquery(myObj);
