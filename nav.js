a378dbfd5d0410eae4f5c58009546d3cb2bd81c8
shameljij <shamel.jij@gmail.com>
2023-08-01 14:10:54 +0200

    added osi and abk

---
+++ nav.js
@@ -0,0 +1,15 @@
+function activeStart(){
+  document.getElementById("startNav").className = "nav-link active";
+  document.getElementById("osiNav").className = "nav-link";
+  document.getElementById("abkNav").className = "nav-link";
+}
+function activeOsi(){
+  document.getElementById("startNav").className = "nav-link";
+  document.getElementById("osiNav").className = "nav-link active";
+  document.getElementById("abkNav").className = "nav-link";
+}
+function activeAbk(){
+  document.getElementById("startNav").className = "nav-link";
+  document.getElementById("osiNav").className = "nav-link";
+  document.getElementById("abkNav").className = "nav-link active";
+}
