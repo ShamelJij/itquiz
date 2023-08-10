065b4f6d800a4ce4863afadd336c8b8f3208360f
shameljij <shamel.jij@gmail.com>
2023-08-10 08:08:59 +0200

    app.js needs corrrection

--- app.js
+++ app.js
@@ -1,44 +1,122 @@
+let myJsons = [
+  "qanda",
+  "abk",
+  "osi",
+  "abkur",
+  "cmd",
+  "security",
+  "fileformat",
+  "industrie",
+  "ipv6",
+  "ki",
+  "pm",
+  "paradigm",
+  "sql",
+  "vgm",
+  "wkp",
+];
+let myPages = {};
+for (var i = 0; i < myJsons.length; i++) {
+  myPages[myJsons[i]] = myJsons[i] + ".json";
+}
+console.log(myPages);
+let responseObj = {};
 let request = new XMLHttpRequest();
-request.open("GET", "answersArray.json", false);
-request.send(null);
-let myArray = JSON.parse(request.responseText);
+for (var i = 0; i < myJsons.length; i++) {
+  request.open("GET", myPages[myJsons[i]], false);
+  request.send(null);
+  responseObj[myJsons[i]] = JSON.parse(request.responseText);
+  // console.log(request.responseText);
+}
+let anchorElements = {};
+let anchors = document.getElementById("anchors");
+for (var i = 0; i < myJsons.length; i++) {
+  anchorElements[myJsons[i] + "TabDiv"] = document.createElement("a");
+  anchorElements[myJsons[i] + "TabDiv"].setAttribute("id", myJsons[i] + "Tab");
+  anchorElements[myJsons[i] + "TabDiv"].innerText = myJsons[i];
+  anchorElements[myJsons[i] + "TabDiv"].className = "nav-link";
+  anchorElements[myJsons[i] + "TabDiv"].style.cursor = "pointer";
+  console.log(anchorElements[myJsons[i] + "TabDiv"]);
+  anchors.appendChild(anchorElements[myJsons[i] + "TabDiv"]);
+}
+//let anchorElements = document.getElementsByTagName("a");
+let startArray = ["start"];
+let tabs = [...startArray, ...myJsons];
+console.log(tabs);
+let tabIds = [];
+for (let i = 0; i < anchorElements.length; i++) {
+  tabIds[i] = anchorElements[i].id;
+}
+console.log(tabIds);
+function n(tabId) {
+  let tab = tabId + "Tab";
+  console.log(tab);
+  for (let i = 0; i < tabs.length - 1; i++) {
+    if (tabs[i] + "Tab" === tab) {
+      document.getElementById(tab).className = "container";
+      console.log(tabs[i]);
+    } else {
+      document.getElementById(tabs[i] + "Tab").className = "d-none";
+      console.log("doesn't!!");
+    }
+  }
+}
+for (let i = 0; i < anchorElements.length; i++) {
+  anchorElements[i].onclick = function () {
+    n(tabIds[i]);
+  };
+}
+console.log(responseObj);
+let QandA = responseObj.start;
 let title_element = document.getElementById("title");
 let question_element = document.getElementById("question");
 let answer_element = document.getElementById("answer");
-let ranMax = myArray.length;
+let ranMax = responseObj.qanda.length;
 
-function getRanId(arr){
+function getRanId(arr) {
   const randomIndex = Math.round(Math.random() * arr.length);
-  if (randomIndex === 0 ){
+  if (randomIndex === 0) {
     return arr[randomIndex].id;
   }
-  return arr[(randomIndex) - 1].id;
+  return arr[randomIndex - 1].id;
 }
-function showEle(){
-  const ranID = getRanId(myArray);
-  console.log("Q_ID: ",ranID);
-  let ranTitle = (JSON.stringify(myArray.find((obj) => obj.id === ranID).title));
-  let ranAnswer = (JSON.stringify(myArray.find((obj) => obj.id === ranID).answer).replace(/^["'](.+(?=["']$))["']$/, '$1'));
-  let ranQuestion = (JSON.stringify(myArray.find((obj) => obj.id === ranID).question).replace(/^["'](.+(?=["']$))["']$/, '$1'));
+function showQandA() {
+  const ranID = getRanId(responseObj.qanda);
+  console.log("Q_ID: ", ranID);
+  let ranTitle = JSON.stringify(
+    responseObj.qanda.find((obj) => obj.id === ranID).title
+  );
+  let ranAnswer = JSON.stringify(
+    responseObj.qanda.find((obj) => obj.id === ranID).answer
+  ).replace(/^["'](.+(?=["']$))["']$/, "$1");
+  let ranQuestion = JSON.stringify(
+    responseObj.qanda.find((obj) => obj.id === ranID).question
+  ).replace(/^["'](.+(?=["']$))["']$/, "$1");
   title_element.innerHTML = "";
-  if (ranTitle === "1"){
-    title_element.innerHTML = "<small>Beurteilen marktgängiger IT-Systeme und Lösungen</small>";
-  } else if (ranTitle === "2"){
-    title_element.innerHTML = "<small>Entwickeln, Erstellen und Betereuen von IT-Lösungen</small>";
-  } else if (ranTitle === "3"){
-    title_element.innerHTML = "<small>Planen, Vorbreiten und Durchführen von Arbeitsaufgaben</small>";
-  } else if (ranTitle === "4"){
-    title_element.innerHTML = "<small>Auftragsabschluss und Leistungserbringung</small>";
-  } else if (ranTitle === "5"){
+  if (ranTitle === "1") {
+    title_element.innerHTML =
+      "<small>Beurteilen marktgängiger IT-Systeme und Lösungen</small>";
+  } else if (ranTitle === "2") {
+    title_element.innerHTML =
+      "<small>Entwickeln, Erstellen und Betereuen von IT-Lösungen</small>";
+  } else if (ranTitle === "3") {
+    title_element.innerHTML =
+      "<small>Planen, Vorbreiten und Durchführen von Arbeitsaufgaben</small>";
+  } else if (ranTitle === "4") {
+    title_element.innerHTML =
+      "<small>Auftragsabschluss und Leistungserbringung</small>";
+  } else if (ranTitle === "5") {
     title_element.innerHTML = "<small>Qualitätssichernde Maßnahmen</small>";
-  } else if (ranTitle === "6"){
-    title_element.innerHTML = "<small>Informieren und Beraten von Kunden und Kundinnen</small>";
-  } else if (ranTitle === "7"){
-    title_element.innerHTML = "<small>IT-Sicherheit und Datenschutz, Ergonomie</small>";
+  } else if (ranTitle === "6") {
+    title_element.innerHTML =
+      "<small>Informieren und Beraten von Kunden und Kundinnen</small>";
+  } else if (ranTitle === "7") {
+    title_element.innerHTML =
+      "<small>IT-Sicherheit und Datenschutz, Ergonomie</small>";
   }
   question_element.innerHTML = "";
   question_element.innerHTML = `${ranQuestion}`;
   answer_element.innerHTML = "";
   answer_element.innerHTML += `${ranAnswer}`;
 }
-showEle();
+showQandA();
