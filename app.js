b07f9ff9daf3c8cd8be5fc2117f489154e0ff830
shameljij <shamel.jij@gmail.com>
2024-06-07 15:05:20 +0200

    corrected typo

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ for (var i = 0; i < myJsons.length; i++) {
   document.getElementById(showTabContentId);
   anchors.appendChild(anchorElements[myJsons[i] + "TabDiv"]);
 }
+
 let tabContentIds = [];
 for (let i = 0; i < myJsons.length; i++) {
   tabContentIds[i] = myJsons[i] + "TabContent";
@@ -107,11 +108,13 @@ const searchList = document.getElementById("resultsList");
 const searchingDiv = document.getElementById("resultDiv");
 const searchSize = document.getElementById("resultSize");
 let searchTimeout;
+
 function preformSearch(){
   const searchValue = searchInput.value.toLowerCase().trim();
   const filteredData = responseObj["qanda"].filter((item) => item.question.toLowerCase().trim().includes(searchValue) || item.answer.toLowerCase().trim().includes(searchValue));
   displayResults(filteredData, searchValue);
 }
+
 function handleSearchInputChange(){
   clearTimeout(searchTimeout);
   if(searchInput.value == ""){
@@ -121,6 +124,7 @@ function handleSearchInputChange(){
       }
   searchTimeout = setTimeout(preformSearch, 1000);
 }
+
 function showSearchingDiv(){
   return new Promise((resolve, reject) => {
     setTimeout(()=> {
@@ -132,22 +136,15 @@ function showSearchingDiv(){
       resolve();
     }, 20);
   });
+
 }
 searchInput.addEventListener("input", () => {
+
   showSearchingDiv().then(() => handleSearchInputChange()).catch((error) => console.error("searching error", error));
 
-    
-          
-            
-    
-
-          
-          Expand Down
-    
-    
-  
   //console.log(filteredData);
 });
+
 function displayResults(results, input) {
   searchList.textContent = "";
   searchSize.textContent = "";
@@ -173,6 +170,7 @@ function displayResults(results, input) {
     //searchList.innerText = resultDiv;
     //console.log(results);
     console.log("searching");
+
     searchList.appendChild(resultDiv);
   });
 }
@@ -530,4 +528,5 @@ function showFirewall() {
     firewall_threats_element.innerHTML += `<li>${ranThreats[i]}</li>`;
   }
 }
+
 showQandA();
